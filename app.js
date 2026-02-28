0f4104f3401c0f2fec91c759a8cd9c1539f41b7d
Madalynn-p <mjpendergr42@tntech.edu>
2026-02-28 12:53:08 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,2 @@
+const weatherUrl = "https://api.open-meteo.com/v1/forecast?latitude=36.1682&longitude=-85.5016&current=temperature_2m,relative_humidity_2m,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago";
+
